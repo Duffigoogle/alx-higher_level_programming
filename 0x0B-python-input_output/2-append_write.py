5b46f4fb1f0ed83af35cf33ed0c50ea9b7c26e8b
duffigoogle <calebdduff@gmail.com>
2022-07-07 04:14:29 +0000

    Add project files for Task 2, 3, 4, 5 & 6

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""Append write"""
+"""4. Append to a file"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """reads n lines of a text file
+def append_write(filename="", text=""):
+    """ appends a string at the end of a text file (UTF8)
     Args:
-        filename (str, optional): text file
-        nb_lines (int, optional): must be integer
+        filename (str, optional): text file name
+        text (str, optional): must be string
+    Returns:
+        [int]: the number of characters added
     """
-    with open(filename, encoding='utf-8') as file:
-        num_lines = sum(1 for line in file)
-
-        file.seek(0)
-
-        if nb_lines <= 0 or nb_lines >= num_lines:
-            print(file.read(), end="")
-            return
-
-        i = 0
-        for i in range(nb_lines):
-            print(file.readline(), end="")
+    with open(filename, mode='a', encoding='utf-8') as file:
+        return file.write(text)
