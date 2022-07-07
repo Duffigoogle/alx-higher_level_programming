9afec131c5c4ea3b4ed7c773fdebd91bd1c830e6
duffigoogle <calebdduff@gmail.com>
2022-07-07 07:55:34 +0000

    Fix Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,23 +3,23 @@
 
 
 def add_integer(a, b=98):
-    """My addition function
-    Parameters:
-        a: first integer
-        b: second integer which have a default value 98
-    Raise:
-        TypeError: If a or b are not integers or float
-    Returns:
-        The return value. a + b
     """
-
-    if type(a) != int and type(a) != float:
+    Add two integers.
+    Parameters:
+        a: must be integer or float
+        b: must be intefer or float
+    Raise:
+        TypeError: If a or b are not integers or floats
+    Returns:
+        Integer: The addition of a and b
+    """
+    if not (type(a) is int or type(a) is float):
         raise TypeError("a must be an integer")
-    elif type(b) != int and type(b) != float:
+    if not (type(b) is int or type(b) is float):
         raise TypeError("b must be an integer")
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
+    if a+1 == a:
+        raise OverflowError("a too large")
+    if b+1 == b:
+        raise OverflowError("b too large")
 
-    return a + b
+    return int(a) + int(b)
