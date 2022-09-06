bd7c45fb43bc5759d576ead64f2de3dae365e350
Ini. Duff Caleb / <calebdduff@gmail.com>
2022-09-06 05:40:43 +0100

    Add project files for Task 8

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let num = parseInt(process.argv[2]);
+let num = parseInt(process.argv[2], 10);
 if (!isNaN(num) && num > 0) {
   for (num; num !==0; num--) {
     // const element = n[index];
