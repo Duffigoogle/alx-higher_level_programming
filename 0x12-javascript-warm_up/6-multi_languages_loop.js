b92e1c00b30b9fa3806134cb5bab58425d404f73
duffigoogle <calebdduff@gmail.com>
2022-09-06 04:15:05 +0000

    Fix task 6 files

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (let index = 0; index < arr.length; index++) {
-    const element = array[index];   
+const Arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let index = 0; index < Arr.length; index++) {
+    const element = Arr[index];
+	console.log(element);
 }
