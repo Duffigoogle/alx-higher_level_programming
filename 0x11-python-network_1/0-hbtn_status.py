fc3bab1f4a60d55ffcefeab07a3bcceacfe54ec1
Ini. Duff Caleb / <calebdduff@gmail.com>
2022-09-24 04:22:32 +0100

    fix Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-# script that fetches https://intranet.hbtn.io/status
+# Script that fetches https://intranet.hbtn.io/status
 import urllib.request
 import urllib.error
 
 try:
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         req = response.read()
-        print("Body response:)
+        print("Body response:")
         print("\t- type: {}".format(type(req)))
         print("\t- content: {}".format(req))
         print("\t- utf8 content: {}".format(req.decode('utf-8', "replace")))
