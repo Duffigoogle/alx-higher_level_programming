556f91db6a715f46eed0e548909ae6fe9bbc755a
Ini. Duff Caleb / <calebdduff@gmail.com>
2022-09-24 04:47:04 +0100

    Fix Task 0,4,7

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,8 @@
 if __name__ == "__main__":
     import requests
 
-    r = requests.get('https://intranet.hbtn.io/status')
+    res = requests.get('https://intranet.hbtn.io/status')
 
     print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    print('\t- type: {}'.format(type(res.text)))
+    print('\t- content: {}'.format(res.text))
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,12 +2,12 @@
 """Takes in a URL, sends a request to the URL and
 displays the body of the response"""
 
-import requests
+from requests import get 
 from sys import argv
 
 if __name__ == "__main__":
     response = requests.get(argv[1])
     if response.status_code >= 400:
-        print("Error code: {}".format(res.status_code))
+        print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
