4295630bcf7aff6dba4ec9f51003cd9a4843fa1c
duffigoogle <calebdduff@gmail.com>
2022-09-24 02:53:20 +0000

    Add project files for Task 0-8

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# script that fetches https://intranet.hbtn.io/status
+import urllib.request
+import urllib.error
+
+try:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        req = response.read()
+        print("Body response:)
+        print("\t- type: {}".format(type(req)))
+        print("\t- content: {}".format(req))
+        print("\t- utf8 content: {}".format(req.decode('utf-8', "replace")))
+except urllib.error.URLError as e:
+    print(e)
---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+"""Takes in Github credentials (username and password) and uses the Github API
+to display an id"""
