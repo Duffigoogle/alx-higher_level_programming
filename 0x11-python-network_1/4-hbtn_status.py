8e46c1b007aabf3398ed0928994af2705ddc3f20
Ini. Duff Caleb / <calebdduff@gmail.com>
2022-09-24 05:25:30 +0100

    Add fixes

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     import requests
 
     res = requests.get('https://intranet.hbtn.io/status')
-
     print('Body response:')
     print('\t- type: {}'.format(type(res.text)))
     print('\t- content: {}'.format(res.text))
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,7 +2,7 @@
 """Takes in a URL, sends a request to the URL and
 displays the body of the response"""
 
-from requests import get 
+import requests
 from sys import argv
 
 if __name__ == "__main__":
