f537d90f2aa4863fb2484284467f51da02a7371b
Ini. Duff Caleb / <calebdduff@gmail.com>
2022-09-24 05:57:04 +0100

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,6 @@
 
 if __name__ == "__main__":
     import requests
-
     res = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
     print('\t- type: {}'.format(type(res.text)))
