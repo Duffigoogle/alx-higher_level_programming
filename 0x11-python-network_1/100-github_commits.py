8d1c989f99cda6107e973a00aeb744bc789dc838
duffigoogle <calebdduff@gmail.com>
2022-09-26 16:28:44 +0000

    Fix codes for Tasks 0, 4, 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,10 +7,11 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2], sys.argv[1]))
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(sys.argv[2], sys.argv[1]))
     if r.status_code >= 400:
         print('None')
     else:
         for com in r.json()[:10]:
             print("{}: {}".format(com.get('sha'),
-            com.get('commit').get('author').get('name')))
+                                  com.get('commit').get('author').get('name')))
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter and print error code"""
+    http://0.0.0.0:5000/search_user with the
+    letter as a parameter and print error code
+"""
 
 
 if __name__ == "__main__":
