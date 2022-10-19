b5ce998751499a83bdd96591c43231498f3fd18b
duffigoogle <calebdduff@gmail.com>
2022-10-19 04:38:58 +0000

    Add project file for Task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// Script that computes the number of tasks completed by user id.
+// ./6-completed_tasks.js https://jsonplaceholder.typicode.com/todos
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const dict = JSON.parse(body).reduce((acc, elem) => {
+    if (!acc[elem.userId]) {
+      if (elem.completed) {
+        acc[elem.userId] = 1;
+      }
+    } else {
+      if (elem.completed) {
+        acc[elem.userId] += 1;
+      }
+    }
+    return acc;
+  }, {});
+  console.log(dict);
+});
